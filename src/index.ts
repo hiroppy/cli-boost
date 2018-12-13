7a2ff5d9e1bbaaaad59a7cbe2b192951f86addaf
Yuta Hiroto <git@about-hiroppy.com>
2018-12-13 11:32:18 +0900

    feat: add core code

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import rc = require('rc');
+import * as ora from 'ora';
+import * as chalk from 'chalk';
+import * as inquirer from 'inquirer';
+import * as configstore from 'configstore';
+import * as commander from 'commander';
+import * as table from 'cli-table2';
+
+export {
+  rc,
+  ora,
+  chalk,
+  inquirer,
+  commander,
+  configstore,
+  table
+}
